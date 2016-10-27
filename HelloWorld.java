751bb625748c130d276918164fd0aead4133d39c
Poulbz <Poul@lawn-143-215-89-254.lawn.gatech.edu>
2016-10-27 19:56:02 -0400

    create

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Apple{
+int i;
+
+public Apple(){
+
+		i=2;
+
+	}
+
+}
+
+
+public class HelloWorld
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		Apple a=new Apple();
+		
+		System.out.println(a.i);
+	}
+}
